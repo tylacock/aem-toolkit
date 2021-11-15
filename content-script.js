90eff1ea8aba7b1e41e68552a5e232c7ec5730de
tylacock <tylacocK@gmail.com>
2021-11-14 23:13:30 -0500

    added open all button in country dropdown

--- content-script.js
+++ content-script.js
@@ -7,7 +7,7 @@ let primaryToolBarLocation = document.getElementsByClassName('_coral-ActionBar-p
 primaryToolBarLocation.insertAdjacentHTML('beforeend', '<a href="#" id="openInLive1" class="btn-link">Live</a>');
 primaryToolBarLocation.insertAdjacentHTML('beforeend', '<a href="#" id="editIntl1" class="btn-link">Country</a>');
 primaryToolBarLocation.insertAdjacentHTML('beforeend', '<a href="#" id="openContentTree1" class="btn-link">Content</a>');
-primaryToolBarLocation.insertAdjacentHTML('beforeend', '<div class="dropdown"><button id="dropdown" class="dropbtn btn-link" ">Dropdown<i class="fa fa-caret-down"></i></button><div class="dropdown-content" id="myDropdown"><a href="#" class="countryLink">EN-US</a><a href="#" class="countryLink">EN-GB</a><a href="#" class="countryLink">EN-IN</a><a href="#" class="countryLink">DE-DE</a><a href="#" class="countryLink">FR-FR</a><a href="#" class="countryLink">IT-IT</a><a href="#" class="countryLink">KO-KR</a><a href="#" class="countryLink">JA-JP</a><a href="#" class="countryLink">ZH-TW</a><a href="#" class="countryLink">ZH-CN</a></div></div>');
+primaryToolBarLocation.insertAdjacentHTML('beforeend', '<div class="dropdown"><button id="dropdown" class="dropbtn btn-link" ">Dropdown<i class="fa fa-caret-down"></i></button><div class="dropdown-content" id="myDropdown"><a href="#" class="countryLink">OPEN ALL</a><a href="#" class="countryLink">EN-US</a><a href="#" class="countryLink">EN-GB</a><a href="#" class="countryLink">EN-IN</a><a href="#" class="countryLink">DE-DE</a><a href="#" class="countryLink">FR-FR</a><a href="#" class="countryLink">IT-IT</a><a href="#" class="countryLink">KO-KR</a><a href="#" class="countryLink">JA-JP</a><a href="#" class="countryLink">ZH-TW</a><a href="#" class="countryLink">ZH-CN</a></div></div>');
 
 
 // Function that opens current AEM page in Live site
@@ -87,6 +87,20 @@ for (var i = 0, length = countryButton.length; i < length; i++) {
         // `this` refers to the anchor tag that's been clicked
         let path = (PAGE_URL.substr(89));
         let countryId = this.innerText;
-        window.open(`https://author-p16153-e39454.adobeaemcloud.com/editor.html/content/ansysincprogram/${countryId.toLowerCase()}/${path}`, '_blank');
+
+
+        if (countryId === "OPEN ALL") {
+            let country = PAGE_URL.substr(83, 5);
+            const countryCodes = ["en-us", "en-in", "en-gb", "fr-fr", "de-de", "it-it", "ja-jp", "ko-kr", "zh-cn", "zh-tw"];
+
+            for (let i = 0; i < countryCodes.length; i++) {
+                if (country !== countryCodes[i]) {
+                    window.open(`https://author-p16153-e39454.adobeaemcloud.com/editor.html/content/ansysincprogram/${countryCodes[i]}/${path}`, '_blank');
+                }
+            }
+        } else {
+            window.open(`https://author-p16153-e39454.adobeaemcloud.com/editor.html/content/ansysincprogram/${countryId.toLowerCase()}/${path}`, '_blank');
+        }
+
     }, true);
 };
