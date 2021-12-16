8f78af2f2a854400ca21f492d9f2f1198c2b0331
tylacock <tylacock@gmail.com>
2021-12-15 23:55:26 -0500

    Added feature to default the editor sidebar to Content tree instead of asset view

--- content-script.js
+++ content-script.js
@@ -1,11 +1,46 @@
 const PAGE_URL = location.href;
-const PRIMARY_TOOLBAR_LOCATION = document.getElementsByClassName('_coral-ActionBar-primary')[0];
 
+
+/* -------------------------------------------------------------
+Onload tweaks
+-------------------------------------------------------------*/
+
+// Adds button toolbar to editor page
+const PRIMARY_TOOLBAR_LOCATION = document.getElementsByClassName('_coral-ActionBar-primary')[0];
 
 PRIMARY_TOOLBAR_LOCATION.insertAdjacentHTML('beforeend', '<a id="openInLive1" class="btn-link">Open in Live</a>');
 PRIMARY_TOOLBAR_LOCATION.insertAdjacentHTML('beforeend', '<a id="openContentTree1" class="btn-link">Open in Content Tree</a>');
 PRIMARY_TOOLBAR_LOCATION.insertAdjacentHTML('beforeend', '<div class="dropdown"><button id="dropdown" class="dropbtn btn-link">Country</button><div class="dropdown-content" id="myDropdown"><a class="countryLink">OPEN ALL</a><a class="countryLink">EN-US</a><a class="countryLink">EN-GB</a><a class="countryLink">EN-IN</a><a class="countryLink">DE-DE</a><a class="countryLink">FR-FR</a><a class="countryLink">IT-IT</a><a class="countryLink">KO-KR</a><a class="countryLink">JA-JP</a><a class="countryLink">ZH-TW</a><a class="countryLink">ZH-CN</a></div></div>');
 
+// Defaults editor toolbar to be on Content Tree instead of image viewer
+
+/*
+1. Select the content tree view (id="coral-id-772")
+2. Add .is-selected to content tree view
+3. Remove .is-selected class to Asset view tab id="coral-id-770"
+*/
+
+window.onload = function() {
+    // const contentTreeTab = document.querySelector('#coral-id-772')
+    // console.log(contentTreeTab)
+    // const assetTab = document.querySelector('#coral-id-770')
+    // console.log(assetTab)
+
+
+    // contentTreeTab.classList.add("is-selected")
+    // assetTab.classList.remove("is-selected")
+
+    const testTab = document.getElementById("coral-id-772")
+    console.log(`testTab =======> ${testTab}`)
+    testTab.click();
+
+
+
+}
+
+/* -------------------------------------------------------------
+Toolbar button functions
+-------------------------------------------------------------*/
 
 // Function that opens current AEM page in Live site
 function openInLive() {
@@ -39,7 +74,7 @@ function openContentTree() {
     window.open(PAGE_URL.replace('/editor.html/', '/sites.html/').slice(0, -5), '_blank');
 }
 
-
+// helper function to toggle the country dropwdown
 function toggleDropdown() {
     /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -55,21 +90,6 @@ toggle between hiding and showing the dropdown content */
         }
     }
 }
-
-
-
-// Listeners
-openInLive1.addEventListener("click", async() => {
-    openInLive();
-});
-
-openContentTree1.addEventListener("click", async() => {
-    openContentTree();
-});
-
-dropdown.addEventListener("click", async() => {
-    toggleDropdown();
-});
 
 // Opens AEM page in corresponding country page.
 let countryButton = document.getElementsByClassName("countryLink");
@@ -96,3 +116,20 @@ for (var i = 0, length = countryButton.length; i < length; i++) {
 
     }, true);
 };
+
+
+
+/* -------------------------------------------------------------
+Listeners
+-------------------------------------------------------------*/
+openInLive1.addEventListener("click", async() => {
+    openInLive();
+});
+
+openContentTree1.addEventListener("click", async() => {
+    openContentTree();
+});
+
+dropdown.addEventListener("click", async() => {
+    toggleDropdown();
+});
